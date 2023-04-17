5036a32f976a325151b29e7ac17191bc164f2392
Masa <mariia.gavrylenko@student.gyarab.cz>
2023-04-17 22:14:42 +0200

    fix classes

--- Rocnikovka_PG/Classes.py
+++ Rocnikovka_PG/Classes.py
@@ -1,8 +1,30 @@
 import pygame
-from rocnikovka import colors, font, stage, screen
+colors = {
+    'background': '0x0F4C75',
+    'buttons': '0x1B262C',
+    'card_bg': '0x00B7C2',
+    'btn_hover': '0x00B7C2',
+    'text': '0xFDCB9E'
+}
+
+colors = {
+    'background': '0x112B3C',
+    'buttons': '0x205375',
+    'card_bg': '0xF66B0E',
+    'btn_hover': '0xF66B0E',
+    'text': '0xEFEFEF'
+}
+
+colors = {
+    'background': '0x222222',
+    'buttons': '0x434242',
+    'card_bg': '0x22A39F',
+    'btn_hover': '0x22A39F',
+    'text': '0xF3EFE0'
+}
 
 class Button():
-    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False):
+    def __init__(self, x, y, width, height, buttonText='Button', onclickFunction=None, onePress=False, screen = None, stage = None, hidd_text = None):
         self.x = x
         self.y = y
         self.width = width
@@ -11,6 +33,9 @@ class Button():
         self.onePress = onePress
         self.alreadyPressed = False
         self.buttonText = buttonText
+        self.screen = screen
+        self.stage = stage
+        self.hidd_text = hidd_text
 
         self.fillColors = {
             'normal' : colors["buttons"],
@@ -21,6 +46,7 @@ class Button():
         self.buttonSurface = pygame.Surface((self.width, self.height))
         self.buttonRect = pygame.Rect(self.x, self.y, self.width, self.height)
 
+        font = pygame.font.SysFont('comicsans', 30,'0xF9D949', True)
         self.buttonSurf = font.render(str(self.buttonText), True,colors["text"])
         
     def process(self):
@@ -38,17 +64,18 @@ class Button():
             else:
                 self.alreadyPressed = False
 
-        if self.buttonText == stage:
+        if self.buttonText == self.stage or self.hidd_text == self.stage:
             self.buttonSurface.fill(self.fillColors['pressed'])
+        
 
         self.buttonSurface.blit(self.buttonSurf, [
         self.buttonRect.width/2 - self.buttonSurf.get_rect().width/2,
         self.buttonRect.height/2 - self.buttonSurf.get_rect().height/2
     ])
-        screen.blit(self.buttonSurface, self.buttonRect)
+        self.screen.blit(self.buttonSurface, self.buttonRect)
 
 class TextField():
-    def __init__(self, x, y, width, height, textholder="default",isPassword=False,function =None):
+    def __init__(self, x, y, width, height, textholder="default",isPassword=False,function =None, screen = None):
         self.x = x
         self.y = y
         self.width = width
@@ -62,7 +89,8 @@ class TextField():
         self.color_active = colors["btn_hover"]
         self.color_passive = colors["buttons"]
         self.color = colors["buttons"]
-        
+        self.screen = screen
+
         self.textRect = pygame.Rect(self.x, self.y, self.width, self.height)
         self.active = False
         self.isSelected = False
@@ -76,26 +104,28 @@ class TextField():
             text_surface = self.base_font.render(string, True, colors["text"])
         else:
             text_surface = self.base_font.render(self.user_text, True, colors["text"])
-        pygame.draw.rect(screen, self.color , self.textRect)
-        screen.blit(text_surface, (self.textRect.x+self.textRect.w/2-text_surface.get_width()/2, self.textRect.y+30)) 
+        pygame.draw.rect(self.screen, self.color , self.textRect)
+        self.screen.blit(text_surface, (self.textRect.x+self.textRect.w/2-text_surface.get_width()/2, self.textRect.y+30)) 
         self.textRect.w = max(300, text_surface.get_width()+10)    
 
 #img =  pygame.image.load('racecar.png')
 class GameCard():
-    def __init__(self, x, y, width, height, title="game",btn_function =None,game_img =None):
+    def __init__(self, x, y, width, height, title="game",btn_function =None,game_img =None, screen = None):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.title = title
         self.textRect = pygame.Rect(self.x, self.y, self.width, self.height)
-        self.btn = Button(self.x+self.width/4, self.y+self.height+10, self.width*2/4, 45, 'Play', btn_function)
+        self.btn = Button(self.x+self.width/4, self.y+self.height+10, self.width*2/4, 45, "Play", btn_function, False, screen, None, " ")
         self.game_img = game_img
         self.base_font = pygame.font.Font(None,40)
+        self.screen = screen
+
     def draw(self):
         
         text_surface = self.base_font.render(self.title, True, colors["text"])
-        pygame.draw.rect(screen, colors["btn_hover"] , self.textRect)
-        screen.blit(text_surface, (self.textRect.x +self.width /2-text_surface.get_width()/2, self.textRect.y))
+        pygame.draw.rect(self.screen, colors["btn_hover"] , self.textRect)
+        self.screen.blit(text_surface, (self.textRect.x +self.width /2-text_surface.get_width()/2, self.textRect.y))
         
         self.btn.process()
--- Rocnikovka_PG/rocnikovka.py
+++ Rocnikovka_PG/rocnikovka.py
@@ -4,9 +4,11 @@
 #https://www.freecodecamp.org/news/create-a-dictionary-in-python-python-dict-methods/
 #
 #
+
 import pygame
-import Database, Classes
-from Classes import Button 
+import Database
+from Classes import GameCard, Button, TextField, colors
+
 global stage
 global money
 global name 
@@ -18,19 +20,10 @@ pygame.init()
 pygame.font.init()
 WIDTH = 1280
 HEIGHT = 720
-colors = {
-    'background': '0x0F4C75',
-    'buttons': '0x1B262C',
-    'card_bg': '0x00B7C2',
-    'btn_hover': '0x00B7C2',
-    'text': '0xFDCB9E'
-}
-
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
-font = pygame.font.SysFont('comicsans', 30,'0xF9D949', True)
 
 # stage home, login, singin, roulete, slot, coinflip, account, about, deposite, test
 stage = "login"
@@ -40,37 +33,48 @@ roulete_wins = 0
 coin_wins = 0
 slot_wins = 0
 
+
+def update_btns(btns):
+    for btn in btns:
+        btn.stage = stage
+
+
 def set_stage_roulette():
     global stage
     stage = "Roulette"
+    update_btns(nav_bar_btns)
 def set_stage_home():
     global stage
     stage = "Home"
+    update_btns(nav_bar_btns)
 def set_stage_slot():
     global stage
     stage = "Slot"
+    update_btns(nav_bar_btns)
 def set_stage_coinflip():
     global stage
     stage = "Coinflip"
+    update_btns(nav_bar_btns)
 def set_stage_account():
     global stage
     stage = "Account"
+    update_btns(nav_bar_btns)
 def set_stage_about():
     global stage
     stage = "About"
+    update_btns(nav_bar_btns)
 def set_stage_deposite():
     global stage
     stage = "Deposite"
 def set_signin_page():
     global stage
     stage = "signin"
-
 def login_function():
     if Database.authorize(log_user_name_txt.user_text,log_user_password_txt.user_text):
         global stage
         load_data()
         stage ="Home"
-
+        update_btns(nav_bar_btns)
 def sign_function():
     global stage
     if Database.add_user(signin_user_name_txt.user_text, signin_user_password_txt.user_text,signin_user_sec_password_txt.user_text):
@@ -79,7 +83,6 @@ def sign_function():
         stage ="login"
     else:
         print("false")
-
 def load_data():
     global money, name, roulete_wins, coin_wins, slot_wins
 
@@ -90,32 +93,32 @@ def load_data():
     coin_wins = data[3]
     slot_wins = data[4]
     
-    nav_bar_account_btn = Button(1000, 0, 280, 60,f"{name}"+" "+f"{money}"+"$", set_stage_account, False)
+    nav_bar_account_btn = Button(1000, 0, 280, 60,f"{name}"+" "+f"{money}"+"$", set_stage_account, False, screen, stage, "Account")
     nav_bar_btns.append(nav_bar_account_btn)
-
+    
 
 #login variables
 login_page_btns = []
-log_signin_btn = Button(WIDTH/2 - 75, 550, 150, 60, 'Sign In', set_signin_page)
-log_login_btn = Button(WIDTH/2 - 125, 450, 250, 60, 'Login', login_function)
+log_signin_btn = Button(WIDTH/2 - 75, 550, 150, 60, 'Sign In', set_signin_page, False, screen, stage)
+log_login_btn = Button(WIDTH/2 - 125, 450, 250, 60, 'Login', login_function, False, screen, stage)
 login_page_btns.append(log_signin_btn)
 login_page_btns.append(log_login_btn)
 
 login_page_txt = []
-log_user_name_txt = Classes.TextField(WIDTH/2-150, 150, 150, 60, 'username',False,None)
-log_user_password_txt = Classes.TextField(WIDTH/2 -150, 250, 150, 60, 'password',True,login_function)
+log_user_name_txt = TextField(WIDTH/2-150, 150, 150, 60, 'username',False,None, screen)
+log_user_password_txt = TextField(WIDTH/2 -150, 250, 150, 60, 'password',True,login_function, screen)
 login_page_txt.append(log_user_name_txt)
 login_page_txt.append(log_user_password_txt)
 
 #signin variables
 signin_page_btns = []
-signin_btn = Classes.Button(WIDTH/2 - 125, 500, 250, 60, 'Sign In', sign_function)
+signin_btn = Button(WIDTH/2 - 125, 500, 250, 60, 'Sign In', sign_function, False, screen, stage)
 signin_page_btns.append(signin_btn)
 
 signin_page_txt = []
-signin_user_name_txt = Classes.TextField(WIDTH/2 - 75, 150, 150, 60, 'username',False,None)
-signin_user_password_txt = Classes.TextField(WIDTH/2 - 75, 250, 150, 60, 'password',True,None)
-signin_user_sec_password_txt = Classes.TextField(WIDTH/2 - 75, 350, 150, 60, 'second password',True,sign_function)
+signin_user_name_txt = TextField(WIDTH/2 - 75, 150, 150, 60, 'username',False,None, screen)
+signin_user_password_txt = TextField(WIDTH/2 - 75, 250, 150, 60, 'password',True,None, screen)
+signin_user_sec_password_txt = TextField(WIDTH/2 - 75, 350, 150, 60, 'second password',True,sign_function, screen)
 signin_page_txt.append(signin_user_name_txt)
 signin_page_txt.append(signin_user_password_txt)
 signin_page_txt.append(signin_user_sec_password_txt)
@@ -123,11 +126,11 @@ signin_page_txt.append(signin_user_sec_password_txt)
 
 #navigation_bar variables
 nav_bar_btns = []
-nav_bar_home_btn = Classes.Button(0, 0, 180, 60, "Home", set_stage_home, False)
-nav_bar_roulette_btn = Classes.Button(180, 0, 180, 60, "Roulette", set_stage_roulette, False)
-nav_bar_slot_btn = Classes.Button(360, 0, 180, 60, "Slot", set_stage_slot, False)
-nav_bar_coin_btn = Classes.Button(540, 0, 180, 60, "Coinflip", set_stage_coinflip, False)
-nav_bar_about_btn = Classes.Button(720, 0, 180, 60, "About", set_stage_about, False)
+nav_bar_home_btn = Button(0, 0, 180, 60, "Home", set_stage_home, False, screen, stage)
+nav_bar_roulette_btn = Button(180, 0, 180, 60, "Roulette", set_stage_roulette, False, screen, stage)
+nav_bar_slot_btn = Button(360, 0, 180, 60, "Slots", set_stage_slot, False, screen, stage, "Slot")
+nav_bar_coin_btn = Button(540, 0, 180, 60, "Coin Flip", set_stage_coinflip, False, screen, stage, "Coinflip")
+nav_bar_about_btn = Button(720, 0, 180, 60, "About", set_stage_about, False, screen, stage)
 
 nav_bar_btns.append(nav_bar_home_btn)
 nav_bar_btns.append(nav_bar_roulette_btn)
@@ -138,9 +141,9 @@ nav_bar_btns.append(nav_bar_about_btn)
 
 #home_page variables
 home_cards = []
-home_roulette_card = Classes.GameCard(150,100,400,200,"Roulette",set_stage_roulette,None)
-home_slot_card = Classes.GameCard(750,100,400,200,"Slot",set_stage_slot,None)
-home_flipcoin_card = Classes.GameCard(150,425,400,200,"Coin Flip",set_stage_coinflip,None)
+home_roulette_card = GameCard(150,100,400,200,"Roulette",set_stage_roulette,None,screen)
+home_slot_card = GameCard(750,100,400,200,"Slots",set_stage_slot,None, screen)
+home_flipcoin_card = GameCard(150,425,400,200,"Coin Flip",set_stage_coinflip,None, screen)
 home_cards.append(home_roulette_card)
 home_cards.append(home_slot_card)
 home_cards.append(home_flipcoin_card)
